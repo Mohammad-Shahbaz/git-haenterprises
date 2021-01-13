3652b4c6f17047f2f65a95ba379a9f93c1b37e88
Mohammad Shahbaz <khanshahbaz12328@gmail.com>
2021-01-13 19:07:09 +0530

    Angular Router Basics

--- haEnterprises/src/app/app.module.ts
+++ haEnterprises/src/app/app.module.ts
@@ -12,13 +12,22 @@ import { ConstdetailsComponent } from './constdetails/constdetails.component';
 import { DesignService } from './services/design.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     ConstdetailsComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -27,7 +36,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DesignService],
   bootstrap: [AppComponent]
